131f44d20b0082ad77fa2cf8ff5f4fb0fb86fc74
hubertbuczkowski <h_buczkowski@outlook.com>
2019-04-24 22:18:18 +0100

    Start

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends React.Component {
+    render()
+    {
+        return "Hello CurrencyFair. I am tarting from this point"
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
